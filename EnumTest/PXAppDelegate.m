fbc9208e405f2433b88e2f5a1fbba7228172d721
Paddy O'Brien <paddy@500px.com>
2013-03-20 14:59:40 -0400

    A test program to illustrate a bug in Xcode 4.6 format specifiers

--- EnumTest/PXAppDelegate.m
+++ EnumTest/PXAppDelegate.m
@@ -5,45 +5,59 @@
 //  Created by Paddy O'Brien on 2013-03-20.
 //  Copyright (c) 2013 500px. All rights reserved.
 //
+//  Tests that typedefed fixed enums work and are being displayed correctly in Xcode 4.6
 
 #import "PXAppDelegate.h"
 
+// ---- These produce the incorrect Format specifier --- //
+
+//Using the new enum macro
+typedef NS_ENUM(NSInteger, PXEnumTest1) {
+	PXEnumTest1One = -1,
+	PXEnumTest1Two
+};
+
+//Without the enum macro
+typedef enum : NSInteger {
+	PXEnumTest2One = -1,
+	PXEnumTest2Two
+} PXEnumTest2;
+
+// ---- This is the style seen in a lot of old apple headers and appears correctly ---- //
+enum : NSInteger
+{
+    PXEnumTest3One = -1,
+    PXEnumTest3Two
+};
+typedef NSInteger PXEnumTest3;
+
 @implementation PXAppDelegate
 
+// All three of these tests should pass regardless of what version of Xcode you are using
+// The result of inspecting the variables while debugging will differ depending on the version of Xcode
+//
+// In Xcode 4.6 the default format specifers for anything of this form, typedef enum : NSInteger, are wrong
+// Has not been tested with other signed values.
+// rdar://13463793
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+	PXEnumTest1 testOne = PXEnumTest1One;
+	if (testOne != PXEnumTest1One) {
+		NSLog(@"Test One Failed. testOne is %d but should be PXEnumTest1One (%d)", testOne, PXEnumTest1One);
+	}
+	
+	PXEnumTest2 testTwo = PXEnumTest2One;
+	if (testTwo != PXEnumTest2One) {
+		NSLog(@"Test Two Failed. testTwo is %d but should be PXEnumTest2One (%d)", testTwo, PXEnumTest2One);
+	}
+	
+	PXEnumTest3 testThree = PXEnumTest3One;
+	if (testThree != PXEnumTest3One) {
+		NSLog(@"Test Three Failed. testOne is %d but should be PXEnumTest3One (%d)", testThree, PXEnumTest3One);
+	}
+	
+	// Put a breakpoint here
     return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-	// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
