aeb0e782bd115732498cc5c5d5369dd6264fcce7
Paddy O'Brien <paddy@500px.com>
2013-03-20 14:20:25 -0400

    Initial Commit

---
+++ EnumTest/PXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PXAppDelegate.h
+//  EnumTest
+//
+//  Created by Paddy O'Brien on 2013-03-20.
+//  Copyright (c) 2013 500px. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EnumTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EnumTest
+//
+//  Created by Paddy O'Brien on 2013-03-20.
+//  Copyright (c) 2013 500px. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([PXAppDelegate class]));
+	}
+}
